04061844c5c016328dfa791ba7f42a7dee8254d8
Nikolay <isp06@mail.ru>
2021-08-10 12:56:28 +0300

    update

--- analizy.sh
+++ analizy.sh
@@ -3,9 +3,14 @@
 echo "If sestatus enable input 0 and i it's desabled or 1 and i it's enable"
 param=$1
 
+
 if  sestatus  | grep "disabled"  ; then
 if [ $# -eq 1 ]; then
+if [ $EUID -eq 0 ] ; then
 setenforce 1
+else
+sudo setenforce 1
+fi
 echo Servise enable
 else
 echo Servise disabled
@@ -13,7 +18,11 @@ exit=1;
 fi
 else
 if [ $# -eq 1 ]; then
+if [ $EUID -eq 0 ] ; then
 setenforce 0
+else
+sudo setenforce 0
+fi
 echo Servise disable
 exit=1
 else
